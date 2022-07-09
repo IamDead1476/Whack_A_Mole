c1fa0012429d9c00d87f52d02f84776f241aa5eb
Harshit Raj <71581461+IamDead1476@users.noreply.github.com>
2022-07-09 11:59:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const score = document.querySelector("#score");
+const timeLeft = document.querySelector("#time-left");
+
+let result = 0;
+let hitPosition = null;
+let currentTime = 60;
+let timerId = null;
+
+function randomSquare () {
+    squares.forEach(square => {
+        square.classList.remove("mole");
+    });
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)];
+    randomSquare.classList.add("mole");
+
+    hitPosition = randomSquare.id;
+};
+
+squares.forEach(square => {
+    square.addEventListener("mousedown", () => {
+        if(square.id === hitPosition){
+            result++;
+
+            score.textContent = result;
+            hitPosition = null;
+        }
+    });
+});
+
+function moveMole () {
+    timerId = setInterval(randomSquare, 700);
+    console.log(timerId);
+};
+
+moveMole();
+
+function countDown () {
+    currentTime--;
+    timeLeft.textContent = currentTime;
+
+    if(currentTime === 0){
+        clearInterval(countDownTimerId);
+        clearInterval(timerId);
+        alert(`GAME OVER! Your final score is: ${result}`);
+    }
+};
+
+let countDownTimerId = setInterval(countDown, 1000);
